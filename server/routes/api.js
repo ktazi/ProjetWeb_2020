a677be1f2d4ea2d8dce5ad43b5cdce98b45230ca
ktazi <kenza.tazi@efrei.net>
2020-10-21 15:15:08 +0200

    post et get /recette

--- server/routes/api.js
+++ server/routes/api.js
@@ -63,7 +63,20 @@ router.get('/recette', async (req, res) => {
  * route that modify a particular recipe
  */
 
-router.put('/recette', (req, res) => {
+router.put('/recette', async (req, res) => {
+    let input = {title : req.body.title,
+        picture : req.body.picture,
+        userid : req.body.userid,//temporaire
+        steps : ["etape 1", "etape2", "etape3"],//temporaire, juste pour les tests
+        mat:["mat1", "mat2"],//temporaire
+        ing:["ing1","ing2","ing3"],//temporaire
+        tag:["tag1","tag2"],
+        rid: req.body.rid}//temporaire
+    let exists = await client.query({
+        text : "SELECT * FROM recette WHERE rid=$1",
+            values : [input.rid]
+    })
+
     res.json({message : "TODO"})
 })
 
@@ -80,45 +93,46 @@ router.post('/recette', async (req, res) => {
                     ing:["ing1","ing2","ing3"],//temporaire
                     tag:["tag1","tag2"]}//temporaire
     let sql = "INSERT INTO recette (title, picture, userid, nb_not, note) VALUES ( $1, $2, $3,0,0) RETURNING*";
-    await client.query({
+    let rid = await client.query({
         text :sql,
         values : [input.title, input.picture, input.userid]
     });
-    let sql2 = "UPDATE recette SET steps = array_append(steps, $1);"
+    rid = rid.rows[0].rid;
+    let sql2 = "UPDATE recette SET steps = array_append(steps, $1) WHERE rid=$2;"
     for (let i = 0; i < input.steps.length; i++)
     {
         await client.query({
             text :sql2,
-            values : [input.steps[i]]
+            values : [input.steps[i],rid]
         });
     }
-    let sql3 = "UPDATE recette SET mat = array_append(mat, $1);"
+    let sql3 = "UPDATE recette SET mat = array_append(mat, $1) WHERE rid=$2;"
     for (let i = 0; i < input.mat.length; i++)
     {
         await client.query({
             text :sql3,
-            values : [input.mat[i]]
+            values : [input.mat[i],rid]
         });
     }
-    let sql4 = "UPDATE recette SET ing = array_append(ing, $1);"
+    let sql4 = "UPDATE recette SET ing = array_append(ing, $1) WHERE rid=$2;"
     for (let i = 0; i < input.ing.length; i++)
     {
         await client.query({
             text :sql4,
-            values : [input.ing[i]]
+            values : [input.ing[i],rid]
         });
     }
-    let sql5 = "UPDATE recette SET tag = array_append(tag, $1);"
+    let sql5 = "UPDATE recette SET tag = array_append(tag, $1) WHERE rid=$2;"
     for (let i = 0; i < input.tag.length; i++)
     {
         await client.query({
             text :sql5,
-            values : [input.tag[i]]
+            values : [input.tag[i],rid]
         });
     }
     let result = await client.query({
         text :"SELECT * FROM recette WHERE rid=$1",
-        values : [4]
+        values : [rid]
     });
     res.json(result.rows)
 })
