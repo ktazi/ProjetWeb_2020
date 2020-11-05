b02c2ff5d089fb656dd6cced21571716ee68616a
Momothea <62673070+Momothea@users.noreply.github.com>
2020-11-05 13:55:19 +0100

    lol

--- client/Components/recettes.vue
+++ client/Components/recettes.vue
@@ -3,13 +3,15 @@
         <p>If this is on the screen, then I've successfully linked the 2</p>
         <div v-for="recette in recettes" :key="recette.rid">
             <div class="recetteName">
-                {{recette.title}} something
+                {{recette.title}} 
+                
             </div>
-            <div class="recetteImage">
-                    something
+            <div class="recetteImage" > 
+                  <div :style=" { backgroundImage: 'url(' + recette.picture + ')' }  ">  </div>  lll
             </div>
             <div class="recettesCorps">
-                    something
+                   {{recette.note}}
+                   {{recette.steps}}
             </div>
 
         </div>
@@ -22,53 +24,50 @@
 
 
 <script>
+    module.exports = {
+    data(){
+        return {
+            recettes:[]
+        }
+    },
+    async mounted(){
+        const res = await axios.get('/api/recettes')
+        this.recettes = res.data
+        console.log(this.recettes)
+        console.log(this.recettes.length)
+        console.log(this.recettes[0].title[0])
+        console.log("A".charCodeAt(0))
+        
+    },
+    methods:{
+        //tri ordre alphabetique de titres
+        title_sort(){
+            var i,j;
+            var nb = this.recettes.length;
+            for(var i = 0; i < nb; i++){
+                for(j = nb - 1; j > i; j--){
+                    if(this.recettes[j-1].title.charCodeAt(0) > this.recettes[j].title.charCodeAt(0) ){
+                        var temp = this.recettes[j-1];
+                        this.recettes[j-1] = this.recettes[j] ;
+                        this.recettes[j] = temp
+                    }
+                }
+            }
+            console.log(this.recettes)
+        }
+        //tri par notes décroissantes
+    }
+    }
+    </script>
 
-    /*function swap(a,b){
-        temp = a;
-        a = b;
-        b = temp;
-       }*/
-module.exports = {
-   data(){
-       return {
-           recettes:[]
-       }
-   },
-   async mounted(){
-       const res = await axios.get('/api/recettes')
-       this.recettes = res.data
-       console.log(this.recettes)
-       console.log(this.recettes.length)
-       console.log(this.recettes[0].title[0])
-       console.log("A".charCodeAt(0))
-    
-   },
-   methods:{
-       //tri ordre alphabetique de titres
-       title_sort(){
-           var i,j,k;
-           var tab = [];
-           var nb = this.recettes.length;
-           for(var k =0; k < nb ; k++){
-               tab[k] = this.recettes[k]
-           }
-           for(var i = 0; i < nb; i++){
-               for(j = nb - 1; j > i; j--){
-                   if(tab[j-1].title.charCodeAt(0) > tab[j].title.charCodeAt(0) ){
-                       var temp = tab[j-1];
-                       tab[j-1] = tab[j] ;
-                       tab[j] = temp
-                   }
-               }
-           }
-           console.log(tab)
-       }
-       //tri par notes décroissantes
-   }
-}
-function swap(a,b){
-        temp = a;
-        a = b;
-        b = temp;
-       }
-</script>
+<style>
+    .recetteImage {
+        height: 30%;
+        height: 20px;
+        width: 20px;
+        flex-direction: column;
+        margin-bottom: 5px;
+        border-bottom : #aaaaaa 1px solid;
+    }
+</style>
+ 
