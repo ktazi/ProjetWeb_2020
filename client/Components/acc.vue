7f574910de12c3376ab8bbed71cda9a36a87a0f2
Constance <constance.garbado@efrei.fr>
2020-11-19 15:43:28 +0100

    bootstrap

--- client/Components/acc.vue
+++ client/Components/acc.vue
@@ -38,8 +38,9 @@
   <section class="py-5">
     <div class="container">
       <h1 class="display-3 text-center">Bienvenu sur Kitch'</h1><br><hr>
-      <br></br><p class="lead">Pas inspiré pour cuisiner? Kitch' est le site de cuisine qui aide les étudiants à cuisiner !
-        <br>« Recette facile et rapide pas chère pour débutant ». Ca vous fait penser à une recherche google ? Plus besoins de faire une recherche, vous trouverez tout sur notre site! Alors rejoignez nous!
+      <br></br><p class="lead text-center">Pas inspiré pour cuisiner? Kitch' est le site de cuisine qui aide les étudiants à cuisiner !
+        <br>« Recette facile et rapide pas chère pour débutant ». Ca vous fait penser à une recherche google ?
+        <br>Plus besoins de faire une recherche, vous trouverez tout sur notre site! Alors rejoignez nous!
       </p>
     </div>
   </section>
@@ -48,13 +49,21 @@
       <div class="row align-items-center">
         <div class="col-lg-6 order-lg-2">
           <div class="p-5">
-            <img class="img-fluid rounded-pill"  src ="https://images.unsplash.com/photo-1553452118-621e1f860f43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="">
+            <img class="img-fluid rounded-pill" id="image0" src ="https://images.unsplash.com/photo-1553452118-621e1f860f43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="">
           </div>
         </div>
         <div class="col-lg-6 order-lg-1">
-          <div class="p-5">
-            <h2 class="display-4">Oulalala</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+          <div class="pl-0-5">
+            <h2 class="display-3">Fait pour vous</h2><br>
+            <div class=" lead">
+            Il y a des vérités dont on se serait bien passé, mais la vie d’étudiant.e n’est pas simple ! Alors si tu te reconnais dans les vérités suivantes c’est que KITCH' est fait pour toi !
+            <br><br>
+            Toi l’étudiant tu manges 5 fruits et légumes frais… par mois<br>
+            Toi l’étudiant tu peux pas acheter les grandes pizzas… parce qu’elles n’entrent pas dans ton frigo…<br>
+            Toi l’étudiant tu n’achètes que des produits « premier prix »<br>
+            Toi l’étudiant tu as déjà jeté de la vaisselle parce que même le plus fort détergent ne pouvait la sauver !<br>
+            Toi l’étudiant quand tu rentres des courses tu dois faire un plan pour tout rentrer dans ton frigo<br>
+            </p>
           </div>
         </div>
       </div>
@@ -68,8 +77,13 @@
         </div>
         <div class="col-lg-6">
           <div class="p-5">
-            <h2 class="display-4">Ca arrive !!!!! </h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+            <h2 class="display-3">Fait par vous</h2><br>
+            <p class="lead">
+            Nous y voilà, il n’y a plus qu’à!<br>
+            D’accord vous allez nous dire, mais quels plats faire à part les éternelles pâtes au thon que tout bon étudiant mange environ 4 repas sur 5 ? <br><br>
+            Si vous etes en manque d'idées KITCH' est là pour vous, Allez jeter un coup d'oeil sur <em>Les recettes populaires </em>publiées par d'autres étudaint comme vous.<br>
+            Mais si au contraire, vos idées fusent alors KITCH' vous propose de les écrire et de les partager ensuite avec nous.
+            </p>
           </div>
         </div>
       </div>
@@ -83,8 +97,11 @@
         </div>
         <div class="col-lg-6 order-lg-1">
           <div class="p-5">
-            <h2 class="display-4">A venir ....</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+            <h2 class="display-4">Et pourquoi pas?</h2><br>
+            <p class="lead">Pour ne pas etre une quiche en cuisine,<br>
+            Trouvez une casserole propre et choisissez dans notre sélection des recettes faciles, économiques et carrément bonnes.<br><br>
+            Alors n'attendez pas Inscrivez vous, connectez vous et laissez la magie de KITCH' vous transporter.
+            </p>
           </div>
         </div>
       </div>
@@ -121,7 +138,7 @@
             <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHyTt1wEp2pGg/profile-displayphoto-shrink_400_400/0?e=1610582400&v=beta&t=TWXW1Po-cQGCgOt077g6Qz2CLqO5zYbeTZn6qOCZwgw" class="card-img-top">
             <div class="card-body text-center">
               <h5 class="card-title mb-0">Constance Garbado</h5>
-              <div class="card-text text-black-50">Une petite description?</div>
+              <div class="card-text text-black-50">une developpeuse ne crie pas elle URL</div>
             </div>
           </div>
         </div>
@@ -157,7 +174,7 @@
     display: block;
     width: 500px;
     height: 500px;
-    margin: 25px auto;
+    margin: 15px ;
     border-radius: 50%;
   }
   #image2{
